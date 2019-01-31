292cdf5ef5d892b25677fa91e0e56e8a54f19eec
Jose Rodriguez Rivas <JoseRivas823@gmail.com>
2019-01-31 09:18:01 -0800

    Replaced Cubes with Coins

--- COMP-437/Labs/Unity-01/Unity-01/Assets/Scripts/Rotator.cs
+++ COMP-437/Labs/Unity-01/Unity-01/Assets/Scripts/Rotator.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class Rotator : MonoBehaviour
 {
+
+    public Vector3 rotationVector;
+
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
+        transform.Rotate(rotationVector * Time.deltaTime);
     }
 }

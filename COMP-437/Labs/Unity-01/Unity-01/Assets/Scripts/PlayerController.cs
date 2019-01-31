f245f13da78a97e11c8e5faf68f1c4b6f016f05a
Jose Rodriguez Rivas <JoseRivas823@gmail.com>
2019-01-30 19:18:16 -0800

    Finished Tutorial

--- COMP-437/Labs/Unity-01/Unity-01/Assets/Scripts/PlayerController.cs
+++ COMP-437/Labs/Unity-01/Unity-01/Assets/Scripts/PlayerController.cs
@@ -1,16 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed;
+    public Text countText;
+    public Text winText;
 
     private Rigidbody rb;
-    public float speed;
+    private int count;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        count = 0;
+        winText.text = "";
+        SetCountText();
     }
 
     void FixedUpdate()
@@ -28,8 +35,18 @@ public class PlayerController : MonoBehaviour
         if (other.gameObject.CompareTag("Pickup"))
         {
             other.gameObject.SetActive(false);
+            count++;
+            SetCountText();
         }
     }
 
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if(count >= 12)
+        {
+            winText.text = "You Win!";
+        }
+    }
 
 }

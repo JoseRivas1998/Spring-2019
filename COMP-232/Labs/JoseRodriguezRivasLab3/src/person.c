0be5fd37d20a622947c409ea5713042d304ae13c
Jose Rodriguez Rivas <joserivas823@gmail.com>
2019-02-13 11:51:18 -0800

    Downloaded lab3

---
+++ COMP-232/Labs/JoseRodriguezRivasLab3/src/person.c
@@ -0,0 +1,40 @@
+#include "person.h"
+
+LIST *head = NULL, *tail = NULL;
+
+void inputPersonalData(PERSON *person)
+{
+    // TODO Implement the function
+}
+
+void addPersonalDataToDatabase(PERSON *person)
+{
+    // TODO Implement the function
+}
+
+void displayDatabase()
+{
+    // TODO Implement the function
+}
+
+void displayPerson(PERSON *person)
+{
+    // TODO Implement the function
+}
+
+PERSON *findPersonInDatabase(char *name)
+{
+    // TODO Implement the function
+
+    return NULL; // if not found
+}
+
+void removePersonFromDatabase(char *name)
+{
+    // TODO Implement the function
+}
+
+void clearDatabase()
+{
+    // TODO Implement the function
+}

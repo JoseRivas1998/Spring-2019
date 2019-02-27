7c0f164bc953ba8db1802c1fe1fdc76f473ac51a
Jose Rodriguez Rivas <joserivas823@gmail.com>
2019-02-27 12:20:17 -0800

    Setup lab 5

---
+++ COMP-232/Labs/JoseRodriguezRivasLab5/src/scanner_transition_table.c
@@ -0,0 +1,73 @@
+//
+// Lab5
+// scanner_transition_table.c
+//
+// Assumes lectures: 1, 2, 3, 4
+
+#include "scanner.h"
+
+TOKEN *ungottenToken = NULL; // may be used by parser in the next lab
+
+//
+// return token to the input, so it can be analyzed again
+//
+void ungetToken(TOKEN **token)
+{
+    ungottenToken = *token;
+    *token = NULL;
+}
+
+//
+// clean up the token structure
+//
+void freeToken(TOKEN **token)
+{
+    if (*token == NULL)
+        return;
+
+    if ((*token)->strVal != NULL)
+        free((*token)->strVal);
+
+    free(*token);
+
+    *token = NULL;
+}
+
+//
+// check if a collected sequence of characters is a keyword
+//
+void updateTypeIfKeyword(TOKEN *token)
+{
+// TODO Implement the function
+}
+
+
+TRANS_TABLE_TYPE *scanInit()
+{
+    TRANS_TABLE_TYPE *returnTable = NULL;
+
+// TODO Implement the function
+
+    return returnTable;
+}
+
+int findIndexToClass(TRANS_TABLE_TYPE *transitionTable, char c)
+{
+    int class;
+
+// TODO Implement the function
+
+    return class;
+}
+
+TOKEN *scanner(TRANS_TABLE_TYPE *transitionTable)
+{
+    TOKEN *token = NULL;
+
+// TODO Implement the functions
+
+    return token;
+}
+
+
+

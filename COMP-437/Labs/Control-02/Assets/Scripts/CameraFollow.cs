2ac55fbd07dfba1ff8c1a95ea7ec69e9660c97ad
Jose Rodriguez Rivas <JoseRivas823@gmail.com>
2019-03-03 18:39:24 -0800

    Started Working on Control

---
+++ COMP-437/Labs/Control-02/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    public GameObject toFollow;
+    public Vector3 offset;
+    public float speed;
+
+    private Vector3 goalPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        goalPosition = new Vector3();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 gameObjectPosition = toFollow.transform.position;
+        goalPosition = gameObjectPosition + offset;
+        transform.position += (goalPosition - transform.position) / speed;
+        transform.LookAt(gameObjectPosition);
+    }
+}

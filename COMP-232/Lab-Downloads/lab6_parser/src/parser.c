8be8ab9391cde64ff6f33b0e6a8509d86d49d1e6
Jose Rodriguez Rivas <JoseRivas823@gmail.com>
2019-03-06 10:24:17 -0800

    Downloaded Parser Lab

---
+++ COMP-232/Lab-Downloads/lab6_parser/src/parser.c
@@ -0,0 +1,68 @@
+#include "parser.h"
+
+TOKEN *getNextToken(TOKEN **token)
+{
+    freeToken(token);
+    return scannerAdHoc();
+}
+
+NODE *program()
+{
+    NODE *node = malloc(sizeof(NODE));
+    node->type = PROGRAM_NODE;
+    node->leftNode = statement();
+    if (node->leftNode != NULL)
+        node->rightNode = program();
+    return node;
+}
+
+NODE *statement()
+{
+//   TODO: implement
+}
+
+NODE *assignStmt(TOKEN **currToken)
+{
+//   TODO: implement
+}
+
+NODE *repeatStmt(TOKEN **currToken)
+{
+//   TODO: implement
+}
+
+NODE *printStmt(TOKEN **currToken)
+{
+//   TODO: implement
+}
+
+NODE *expr(TOKEN **currToken)
+{
+//   TODO: implement
+}
+
+NODE *term(TOKEN **currToken)
+{
+//   TODO: implement
+}
+
+NODE *factor(TOKEN **currToken)
+{
+//   TODO: implement
+}
+
+NODE *id(TOKEN **currToken)
+{
+//   TODO: implement
+}
+
+NODE *number(TOKEN **currToken)
+{
+//   TODO: implement
+}
+
+void error(char *errorMessage)
+{
+    printf("PARSING ERROR: %s\nExiting...\n", errorMessage);
+    exit(1);
+}

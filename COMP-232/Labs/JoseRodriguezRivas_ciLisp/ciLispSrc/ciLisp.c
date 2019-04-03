31453b88980c5cff10e00c12df9853ca5f626c6e
Jose Rodriguez Rivas <JoseRivas823@gmail.com>
2019-04-03 12:01:01 -0700

    Created Project

---
+++ COMP-232/Labs/JoseRodriguezRivas_ciLisp/ciLispSrc/ciLisp.c
@@ -0,0 +1,101 @@
+#include "ciLisp.h"
+
+void yyerror(char *s) {
+    fprintf(stderr, "\nERROR: %s\n", s);
+    // note stderr that normally defaults to stdout, but can be redirected: ./src 2> src.log
+    // CLion will display stderr in a different color from stdin and stdout
+}
+
+//
+// find out which function it is
+//
+
+char *func[] = {"neg", "abs", "exp", "sqrt", "add", "sub", "mult", "div", "remainder", "log", "pow", "max", "min",
+                "exp2", "cbrt", "hypot", "read", "rand", "print", "equal", "smaller", "larger", ""};
+
+OPER_TYPE resolveFunc(char *funcName)
+{
+    int i = 0;
+    while (func[i][0] != '\0')
+    {
+        if (strcmp(func[i], funcName) == 0)
+            return i;
+
+        i++;
+    }
+    return CUSTOM_FUNC;
+}
+
+//
+// create a node for a number
+//
+AST_NODE *number(double value)
+{
+    AST_NODE *p;
+    size_t nodeSize;
+
+    // allocate space for the fixed sie and the variable part (union)
+    nodeSize = sizeof(AST_NODE) + sizeof(NUMBER_AST_NODE);
+    if ((p = malloc(nodeSize)) == NULL)
+        yyerror("out of memory");
+
+    p->type = NUM_TYPE;
+    p->data.number.value = value;
+
+    return p;
+}
+
+//
+// create a node for a function
+//
+AST_NODE *function(char *funcName, AST_NODE *op1, AST_NODE *op2)
+{
+    AST_NODE *p;
+    size_t nodeSize;
+
+    // allocate space for the fixed sie and the variable part (union)
+    nodeSize = sizeof(AST_NODE) + sizeof(FUNCTION_AST_NODE);
+    if ((p = malloc(nodeSize)) == NULL)
+        yyerror("out of memory");
+
+    p->type = FUNC_TYPE;
+    p->data.function.name = funcName;
+    p->data.function.op1 = op1;
+    p->data.function.op2 = op2;
+
+    return p;
+}
+
+//
+// free a node
+//
+void freeNode(AST_NODE *p)
+{
+    if (!p)
+        return;
+
+    if (p->type == FUNC_TYPE)
+    {
+        free(p->data.function.name);
+        freeNode(p->data.function.op1);
+        freeNode(p->data.function.op2);
+    }
+
+    free(p);
+}
+
+//
+// evaluate an abstract syntax tree
+//
+// p points to the root
+//
+double eval(AST_NODE *p)
+{
+    if (!p)
+        return 0.0;
+
+// TBD: implement
+
+    return 0.0;
+}  
+
